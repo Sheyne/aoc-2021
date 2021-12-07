ce667b50efdec19b37fe0d70a3a1a747e5764891
Sheyne Anderson <sheyne@sheyne.com>
2021-12-07 07:26:59 +0000

    Implement day 3

--- app/Main.hs
+++ app/Main.hs
@@ -2,6 +2,8 @@ module Main where
 
 import System.IO  
 import Control.Monad
+import Data.List
+import Data.Bits
 
 pairwise :: [a] -> [(a, a)]
 pairwise (x:rest) = zip (x:rest) rest
@@ -49,10 +51,65 @@ solveDay2Part2 moves = let (x, y, _) = inner moves in x * y
           applyMove (x, y, aim) (Down daim) = (x, y, aim + daim)
           applyMove (x, y, aim) (Forward d) = (x + d, y + aim * d, aim)
 
-main :: IO ()
-main = do  
-        handle <- openFile "data/day2-input1.txt" ReadMode
-        contents <- hGetContents handle
-        putStrLn $ show $ solveDay2Part2 $ parseDay2 $ contents
-        hClose handle   
+parseDay3 :: String -> [[Int]]
+parseDay3 = map toBits . lines
+    where toBits :: String -> [Int]
+          toBits = map toBit
 
+          toBit :: Char -> Int
+          toBit '0' = 0
+          toBit '1' = 1
+
+
+bitsToInt :: [Int] -> Int
+bitsToInt = rBitsToInt . reverse
+    where     
+        rBitsToInt :: [Int] -> Int
+        rBitsToInt (x : rest) = x + 2 * rBitsToInt rest
+        rBitsToInt [] = 0
+
+solveDay3Part1 :: [[Int]] -> Int
+solveDay3Part1 nums = 
+    let mostlyOnes = let
+                         sums = map sum $ transpose nums
+                         count = length nums
+                     in map (\sum -> sum > count - sum) sums
+    in (bitsToInt (map (\b -> if b then 1 else 0) mostlyOnes)) * (bitsToInt (map (\b -> if b then 0 else 1) mostlyOnes))
+
+solveDay3Part2 :: [[Int]] -> Int
+solveDay3Part2 nums = let 
+                          zips = map toZipList nums
+                          o2 = inner 1 zips
+                          co2 = inner 0 zips
+                      in (bitsToInt o2) * (bitsToInt co2)
+    where toZipList :: [Int] -> ([Int], Int, [Int])
+          toZipList (x:rest) = ([], x, rest)
+          fromZipList :: ([Int], Int, [Int]) -> [Int]
+          fromZipList ([], current, right) = current : right
+          fromZipList (newCurrent:left, current, right) = fromZipList (left, newCurrent, current: right)
+          advance :: ([Int], Int, [Int]) -> ([Int], Int, [Int])
+          advance (left, current, newCurrent:right) = (current:left, newCurrent, right)
+          currents :: [([Int], Int, [Int])] -> [Int]
+          currents = map (\(_, c, _) -> c)
+          select :: Int -> [([Int], Int, [Int])] -> [([Int], Int, [Int])]
+          select a zips = let
+                              numMatching1 = length $ filter (\c -> c == 1) (currents zips)
+                              numMatching0 = (length zips) - numMatching1
+                              goodBit = case (a, compare numMatching0 numMatching1) of (_, EQ) -> a
+                                                                                       (0, LT) -> 0
+                                                                                       (0, GT) -> 1
+                                                                                       (1, GT) -> 0
+                                                                                       (1, LT) -> 1
+                                        
+                          in filter (\(_, c, _) -> c == goodBit) zips
+
+          inner :: Int -> [([Int], Int, [Int])] -> [Int]
+          inner a x = case (select a x) of selected:[] -> fromZipList selected
+                                           selected -> inner a (map advance selected)
+
+main :: IO ()
+main = do
+        handle <- openFile "data/day3.txt" ReadMode
+        contents <- hGetContents handle
+        putStrLn $ show $ solveDay3Part2 $ parseDay3 $ contents
+        hClose handle
