7f76debced935a2bf37d70973d5b9d051d849200
Sheyne Anderson <sheyne@sheyne.com>
2021-12-07 06:06:11 +0000

    Implement day 2

--- app/Main.hs
+++ app/Main.hs
@@ -18,10 +18,41 @@ solveDay1Part1 x = length (filter (\(a, b) -> a < b) $ pairwise x)
 solveDay1Part2 :: [ Int ] -> Int
 solveDay1Part2 x = solveDay1Part1 $ map (\(a, b, c) -> a + b + c) $ triplewise x
 
+data Movement = Up Int | Down Int | Forward Int deriving Show
+
+parseMovement :: String -> Movement
+parseMovement x = case words x of 
+                    ["up", amt] -> Up $ read amt
+                    ["down", amt] -> Down $ read amt
+                    ["forward", amt] -> Forward $ read amt
+
+parseDay2 :: String -> [ Movement ]
+parseDay2 = map parseMovement . lines
+
+solveDay2Part1 :: [ Movement ] -> Int
+solveDay2Part1 moves = let (x, y) = inner moves in x * y
+    where inner :: [ Movement ] -> (Int, Int)
+          inner = foldr applyMove (0, 0)
+
+          applyMove :: Movement -> (Int, Int) -> (Int, Int)
+          applyMove (Up dy) (x, y) = (x, y - dy)
+          applyMove (Down dy) (x, y) = (x, y + dy)
+          applyMove (Forward dx) (x, y) = (x + dx, y)
+
+solveDay2Part2 :: [ Movement ] -> Int
+solveDay2Part2 moves = let (x, y, _) = inner moves in x * y
+    where inner :: [ Movement ] -> (Int, Int, Int)
+          inner = foldl applyMove (0, 0, 0)
+
+          applyMove :: (Int, Int, Int) -> Movement -> (Int, Int, Int)
+          applyMove (x, y, aim) (Up daim) = (x, y, aim - daim)
+          applyMove (x, y, aim) (Down daim) = (x, y, aim + daim)
+          applyMove (x, y, aim) (Forward d) = (x + d, y + aim * d, aim)
+
 main :: IO ()
 main = do  
-        handle <- openFile "data/day1-input1.txt" ReadMode
+        handle <- openFile "data/day2-input1.txt" ReadMode
         contents <- hGetContents handle
-        putStrLn $ show $ solveDay1Part2 $ parseDay1 $ contents
+        putStrLn $ show $ solveDay2Part2 $ parseDay2 $ contents
         hClose handle   
 
