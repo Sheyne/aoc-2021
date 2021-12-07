59ba49d58543f7604add4ab8cf7c40c29161f686
Sheyne Anderson <sheyne@sheyne.com>
2021-12-07 05:38:12 +0000

    Finish day 1

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,27 @@
 module Main where
 
+import System.IO  
+import Control.Monad
+
+pairwise :: [a] -> [(a, a)]
+pairwise (x:rest) = zip (x:rest) rest
+
+triplewise :: [a] -> [(a, a, a)]
+triplewise (x:rest) = map (\(a, (b, c)) -> (a, b, c)) $ zip (x:rest) $ pairwise rest
+
+parseDay1 :: String -> [ Int ]
+parseDay1 x = map read (lines x)
+
+solveDay1Part1 :: [ Int ] -> Int
+solveDay1Part1 x = length (filter (\(a, b) -> a < b) $ pairwise x)
+
+solveDay1Part2 :: [ Int ] -> Int
+solveDay1Part2 x = solveDay1Part1 $ map (\(a, b, c) -> a + b + c) $ triplewise x
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do  
+        handle <- openFile "data/day1-input1.txt" ReadMode
+        contents <- hGetContents handle
+        putStrLn $ show $ solveDay1Part2 $ parseDay1 $ contents
+        hClose handle   
+
