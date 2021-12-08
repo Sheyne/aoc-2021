8df1ae74b98b1a9f61eb090976582c4f839214bc
Sheyne Anderson <sheyne@sheyne.com>
2021-12-08 00:30:29 +0000

    Solve day 4

--- app/Main.hs
+++ app/Main.hs
@@ -4,6 +4,8 @@ import System.IO
 import Control.Monad
 import Data.List
 import Data.Bits
+import Debug.Trace
+import Data.Maybe
 
 pairwise :: [a] -> [(a, a)]
 pairwise (x:rest) = zip (x:rest) rest
@@ -107,9 +109,61 @@ solveDay3Part2 nums = let
           inner a x = case (select a x) of selected:[] -> fromZipList selected
                                            selected -> inner a (map advance selected)
 
+data Board = Board { draws :: [Int], boards :: [[[Int]]] } deriving Show
+
+splitOn :: Eq a => a -> [a] -> [[a]]
+splitOn _ [] = []
+splitOn a s = case break (a ==) s of (x, []) -> [x]
+                                     (x, _:rest) -> x : (splitOn a rest)
+
+parseDay4 :: String -> Board
+parseDay4 x = let
+                  [first] : boards = splitOn "" $ splitOn '\n' x
+              in Board { draws = map read (splitOn ',' first) , boards = map parseBoard boards }
+              where parseBoard :: [String] -> [[Int]]
+                    parseBoard = map parseRow
+                    parseRow :: String -> [Int]
+                    parseRow = map read . filter ("" /=) . words
+
+
+prefixes :: Int -> [a] -> [[a]]
+prefixes n x = take n x : prefixes (n + 1) x
+markBoard :: [Int] -> [[Int]] -> [[Bool]]
+markBoard selected = map $ map $ flip elem selected
+checkBoard :: [[Bool]] -> Bool
+checkBoard x = checkRows x || checkRows (transpose x)
+checkRows :: [[Bool]] -> Bool
+checkRows = any $ all id
+scoreBoard :: [Int] -> [[Int]] -> Int
+scoreBoard called board = (last called) * sum (filter (not . flip elem called) $ concat board)
+
+solveDay4Part1 :: Board -> Int
+solveDay4Part1 b = head $ mapMaybe (solveForPrefix (boards b)) ((prefixes 0 (draws b)))
+    where solveForPrefix :: [[[Int]]] -> [Int] -> (Maybe Int)
+          solveForPrefix boards called = let
+                                             boardCompletions = map checkBoard $ map (markBoard called) boards
+                                             finishedBoards = map fst (filter snd (zip boards boardCompletions))
+                                         in case finishedBoards of [] -> Nothing
+                                                                   board: _ -> Just $ scoreBoard called board
+
+
+solveDay4Part2 :: Board -> Int
+solveDay4Part2 b = step (boards b) (replicate (length (boards b)) False) (prefixes 0 (draws b))
+    where step :: [[[Int]]] -> [Bool] -> [[Int]] -> Int
+          step boards previous (called:nextToCall) = let
+                                                         boardCompletions = map checkBoard $ map (markBoard called) boards
+                                                     in if all id boardCompletions then 
+                                                            let
+                                                                lastBoard = head (map snd (filter (not . fst) (zip previous boards)))
+                                                            in scoreBoard called lastBoard
+                                                        else step boards boardCompletions nextToCall
+
+
+solvePuzzle = solveDay4Part2 . parseDay4
+
 main :: IO ()
 main = do
-        handle <- openFile "data/day3.txt" ReadMode
+        handle <- openFile "data/day4.txt" ReadMode
         contents <- hGetContents handle
-        putStrLn $ show $ solveDay3Part2 $ parseDay3 $ contents
+        putStrLn $ show . solvePuzzle $ contents
         hClose handle
