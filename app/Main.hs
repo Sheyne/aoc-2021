ccc8b69757c9ceda6c47bdd6cfcdb3f6009eddda
Sheyne Anderson <sheyne@sheyne.com>
2021-12-14 09:04:39 +0000

    Solve day 14

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE TupleSections #-}
 module Main where
 
 import System.IO
@@ -627,7 +628,7 @@ gridFromPoints points = runST $ do
 
     mapM_ (\(x, y) -> writeArray arr (y, x) True) points
 
-    sequence [do                           
+    sequence [do
             row <- mapIndices ((y, 0), (y, width)) id arr
             getElems row | y <- [0..height]]
 
@@ -641,11 +642,59 @@ solveDay13Part2 (points, folds) = let grid = gridFromPoints points
                                       folded = foldl (\grid fold-> foldPaper' ('x' == fst fold) (snd fold) grid) grid folds
                                   in unlines $ map (map (\x -> if x then '#' else '.')) folded
 
-solvePuzzle = solveDay13Part2 . parseDay13
+parseDay14 :: String -> (String, [((Char, Char), Char)])
+parseDay14 x = let [[initial], mappings] = splitOn [] $ lines x
+               in (initial,
+                  map (\q -> let [a, b, _, _, _, _, c] = q in ((a, b), c)) mappings)
+
+stepPairInsertion :: [((Char, Char), Char)] -> String -> String
+stepPairInsertion mapping x@(a:_) = a : concatMap (\p@(l, r) ->
+                case find ((== p).fst) mapping of Just (_, i) -> [i, r]
+                                                  Nothing -> [r]) (pairwise x)
+stepPairInsertion _ a = a
+
+solveDay14Part1 :: (String, [((Char, Char), Char)]) -> Int
+solveDay14Part1 (initial, mappings) = let s = applyNTimes 10 (stepPairInsertion mappings) initial
+                                          occurances = sort $ countOccurances s
+                                          (leastCommon, _) = head occurances
+                                          (mostCommon, _) = last occurances
+                                      in mostCommon - leastCommon
+
+joinCounts :: (Eq a) => [(Int, a)] -> [(Int, a)]
+joinCounts [] = []
+joinCounts (x:rest) = inner x (joinCounts rest)
+    where inner :: (Eq a) => (Int, a) -> [(Int, a)] -> [(Int, a)]
+          inner x [] = [x]
+          inner (n1, x) ((n2, y):rest) | x == y = (n1+n2, x) : rest
+          inner x (fst:rest) = fst : inner x rest
+
+
+solveDay14Part2 :: (String, [((Char, Char), Char)]) -> Int
+solveDay14Part2 (initial, mappings) = let
+        (letterCounts, _) = applyNTimes 40 (step mappings) (countOccurances initial, countOccurances (pairwise initial))
+        letterCounts' = sort letterCounts
+        (leastCommon, _) = head letterCounts'
+        (mostCommon, _) = last letterCounts'
+        in mostCommon - leastCommon
+    where pairBecomes :: [((Char, Char), Char)] -> (Char, Char) -> ([(Char, Char)], [Char])
+          pairBecomes mapping pair@(l, r) = case find ((== pair).fst) mapping of
+                                        Just (_, x) -> ([(l, x), (x, r)], [x])
+                                        Nothing -> ([pair], [])
+
+          step :: [((Char, Char), Char)] -> ([(Int, Char)], [(Int, (Char, Char))]) -> ([(Int, Char)], [(Int, (Char, Char))])
+          step mappings (letterCounts, pairCounts) =
+              let newCounts = map (\(count, pair) ->
+                                let (pairs, letters) = pairBecomes mappings pair
+                                in (map (count,) pairs, map (count,) letters)) pairCounts
+                  newPairCounts = joinCounts (concatMap fst newCounts)
+                  newLetterCounts = joinCounts (letterCounts ++ concatMap snd newCounts)
+              in (newLetterCounts, newPairCounts)
+
+solvePuzzle = solveDay14Part2 . parseDay14
 
 main :: IO ()
 main = do
-        handle <- openFile "data/day13.txt" ReadMode
+        handle <- openFile "data/day14.txt" ReadMode
         contents <- hGetContents handle
-        putStrLn $ solvePuzzle contents
+        print $ solvePuzzle contents
         hClose handle
