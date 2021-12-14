ccb3ffb7e5038da8a6f14660f67387a4f3e270d1
Sheyne Anderson <sheyne@sheyne.com>
2021-12-14 01:44:43 +0000

    Part 12 was actually cute in haskell

--- app/Main.hs
+++ app/Main.hs
@@ -6,7 +6,6 @@ import Data.List
 import Data.Maybe
 import Data.Char
 import Control.Monad
-import Text.ParserCombinators.ReadP (count)
 import Control.Arrow
 import Debug.Trace
 import Control.Monad.ST
@@ -557,11 +556,54 @@ solveDay11Part2 x = runST $ do
         modifyAll arr (\case Just x -> Just x ; Nothing -> Just 0)
         pure result
 
-solvePuzzle = solveDay11Part2 . parseDay11
+parseDay12 :: String -> [(String, String)]
+parseDay12 = map (\x -> let [a, b] = splitOn '-' x in (a, b)) . lines
+
+solveDay12Part1 :: [(String, String)] -> Int
+solveDay12Part1 x = length $ visit x [["start"]] "start"
+
+    where neighbors :: [(String, String)] -> String -> [String]
+          neighbors connections key = map fst (filter ((== key).snd) connections) ++ map snd (filter ((== key).fst) connections)
+
+          visit :: [(String, String)] -> [[String]] -> String -> [[String]]
+          visit connections previouses current = do
+                previous <- previouses
+                next <- filter (canFollow previous) $ neighbors connections current
+                if next == "end" then
+                    [next : previous]
+                else
+                    visit connections [ next: previous] next
+
+          canFollow :: [String] -> String -> Bool
+          canFollow _ x | isUpper $ head x = True
+          canFollow previous x = x `notElem` previous
+
+solveDay12Part2 :: [(String, String)] -> Int
+solveDay12Part2 x = length $ visit x [(False, ["start"])] "start"
+
+    where neighbors :: [(String, String)] -> String -> [String]
+          neighbors connections key = map fst (filter ((== key).snd) connections) ++ map snd (filter ((== key).fst) connections)
+
+          visit :: [(String, String)] -> [(Bool, [String])] -> String -> [[String]]
+          visit connections previouses current = do
+                (hasUsed, previous) <- previouses
+                ((hasUsed, _), next) <- filter (snd.fst) $ map (\x -> (canFollow (hasUsed, previous) x, x)) $ neighbors connections current
+                if next == "end" then
+                    [next : previous]
+                else
+                    visit connections [(hasUsed, next: previous)] next
+
+          canFollow :: (Bool, [String]) -> String -> (Bool, Bool)
+          canFollow (hasUsed, _) x | isUpper $ head x = (hasUsed, True)
+          canFollow (hasUsed, _) x | x == "start" = (hasUsed, False)
+          canFollow (False, previous) x = (x `elem` previous, True)
+          canFollow (True, previous) x = (True, x `notElem` previous)
+
+solvePuzzle = solveDay12Part1. parseDay12
 
 main :: IO ()
 main = do
-        handle <- openFile "data/day11.txt" ReadMode
+        handle <- openFile "data/day12.txt" ReadMode
         contents <- hGetContents handle
         print $ solvePuzzle contents
         hClose handle
